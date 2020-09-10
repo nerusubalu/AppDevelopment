78f20b1280dfd4ff7f79074d9c7869a8f5fec932
balu <nerusubalu@gmail.com>
2020-09-10 21:41:20 +0530

    Intial

--- app/src/main/java/com/example/arsmarthome/RoomActivity.kt
+++ app/src/main/java/com/example/arsmarthome/RoomActivity.kt
@@ -105,9 +105,9 @@ class RoomActivity : AppCompatActivity() {
         recycleview.adapter = adapter
         val orientation =resources.configuration.orientation
         if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            recycleview.layoutManager = GridLayoutManager(this, 2)
+            recycleview.layoutManager = GridLayoutManager(this, 3)
         } else {
-            recycleview.layoutManager = GridLayoutManager(this,1)
+            recycleview.layoutManager = GridLayoutManager(this,2)
         }
     }
 
